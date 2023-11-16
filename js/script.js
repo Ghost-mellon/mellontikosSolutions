d36357cb91adfd410692ad77e0772d57592c70a5
Cyber <cybermonk369@gmail.com>
2023-11-17 00:32:24 +0530

    commit with copied files from another folder

---
+++ js/script.js
@@ -0,0 +1,10 @@
+entry = prompt("Enter your Key Here")
+// fool = document.querySelector('#fool')
+// console.log(fool);
+
+if(entry == "monk"){
+    document.getElementById('mainBod').src = window.location.href = "godfatherSecrt.html"; 
+} else {
+     document.getElementById("fool").innerHTML = "Nothing To see Here" 
+}
+
